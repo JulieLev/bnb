f4dc71236ec7c712118ef2fa50855e42a094e569
albiebabie <albiebabie@gmail.com>
2016-08-09 17:43:57 +0100

    add spaces/new.erb and add_space_spec.rb -test failing - Unable to find css "ul#spaces"

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,10 @@ class Bnb < Sinatra::Base
     'Hello BNB!'
   end
 
+  get '/spaces/new' do
+    erb :'spaces/new'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
