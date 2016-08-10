6a0000d69964197ed9a887ee71d06ea38728b4b2
noby <n.fujioka@abacusmaths.info>
2016-08-10 15:01:08 +0100

    adds blank password prevention to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,14 @@ class User
 
   property :id, Serial
   property :name, String, required: true
-  property :password_digest, String, length: 60
+  property :password_digest, String, length: 60, required: true
   property :email, String, required: true, format: :email_address, unique: true
 
 
   attr_reader :password
   attr_accessor :password_confirmation
 
+  validates_presence_of :password
   validates_confirmation_of :password
 
   def password=(my_password)
@@ -22,13 +23,13 @@ class User
     self.password_digest = BCrypt::Password.create(my_password)
   end
 
-  def self.authenticate(email, password)
-    user = first(email: email)
-    if user && BCrypt::Password.new(user.password_digest) == password
-      user
-    else
-      nil
-    end
-  end
+  # def self.authenticate(email, password)
+  #   user = first(email: email)
+  #   if user && BCrypt::Password.new(user.password_digest) == password
+  #     user
+  #   else
+  #     nil
+  #   end
+  # end
 
 end
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -11,4 +11,37 @@ feature 'Signup' do
     expect(page).to have_content('Password does not match the confirmation')
   end
 
+  scenario 'I can not sign up without password supplied' do
+    expect { signup(password: "") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content("Password must not be blank")
+  end
+
+  scenario 'I cannot sign up using a duplicate email address' do
+    signup(email: "mine@duplicate.com")
+    # click_button 'sign out'
+    expect{ signup(email: "mine@duplicate.com") }.not_to change(User, :count)
+    expect(page).to have_content("Email is already taken")
+  end
+
+  scenario 'I can not sign up without entering an email' do
+    expect { signup(email: "") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content("Email must not be blank")
+  end
+
+  scenario 'I can not sign up using an invalid email format' do
+    expect { signup(email: "abcemail.com") }.not_to change(User, :count)
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+
+  # scenario 'I should not see sign up page while I am signed in' do
+  #   signup
+  #   click_button 'sign out'
+  #   signin(username: "AppleMan", password: "apple123")
+  #   visit '/users/new'
+  #   expect(page).not_to have_content "Sign up"
+  # end
+
 end
