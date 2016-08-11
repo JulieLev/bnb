56195192c5a6e554c520d47da1f9a5e82308a31d
Titus Iudean <iudeant@gmail.com>
2016-08-11 14:48:46 +0300

    Adds date to space creation

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,7 +5,9 @@ class Space
   property :space_name, String
   property :description, String
   property :price, Float
-
+  property :date_from, Date
+  property :date_to, Date
+  
   belongs_to :user
 
 end
